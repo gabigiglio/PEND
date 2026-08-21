e7e84dbfe079aefed5263007d5dfae47d81eb33a
gabigiglio <gabriela.giglio@aluno.senai.br>
2026-08-21 07:40:40 -0300

    desafio extra 21/08

--- JavaScript/Orientação a Objetos/OO+DOM/ex1/script.js
+++ JavaScript/Orientação a Objetos/OO+DOM/ex1/script.js
@@ -7,8 +7,8 @@ class Produto {
     }
 
     aplicarDesconto() {
-    const precoFinal = this.preco - (this.preco * this.desconto / 100);
-    return precoFinal;
+        const precoFinal = this.preco - (this.preco * this.desconto / 100);
+        return precoFinal;
     }
 }
 
@@ -21,27 +21,39 @@ class Estoque {
         this.produtos.push(produto);
     }
 
+    excluirProduto(indice) {
+        this.produtos.splice(indice, 1);
+        this.exibirNaTela();
+    }
+
     exibirNaTela() {
         const resultado = document.querySelector('#resultado');
 
         resultado.innerHTML = "";
 
-        this.produtos.forEach(produto => {
+        this.produtos.forEach((produto, indice) => {
 
             resultado.innerHTML += `
-            <div>
-                <p>Nome: ${produto.nome}</p>
-                <p>Preço original: ${produto.preco}</p>
-                <p>Categoria: ${produto.categoria}</p>
-                <p>Desconto: ${produto.desconto}%</p>
-                <p>Preço com desconto: ${produto.aplicarDesconto()}</p>
-            </div>
+                <div class="produto">
+                    <p>Nome: ${produto.nome}</p>
+                    <p>Preço original: R$ ${produto.preco}</p>
+                    <p>Categoria: ${produto.categoria}</p>
+                    <p>Desconto: ${produto.desconto}%</p>
+                    <p>Preço com desconto: R$ ${produto.aplicarDesconto()}</p>
+
+                    <button class="botaoExcluir" onclick="estoque.excluirProduto(${indice})">
+                        Excluir
+                    </button>
+
+                    <hr>
+                </div>
             `;
         });
     }
 }
 
 const estoque = new Estoque();
+
 const nome = document.querySelector('#nome');
 const preco = document.querySelector('#preco');
 const categoria = document.querySelector('#categoria');
@@ -50,12 +62,19 @@ const botaoCadastrar = document.querySelector('#botaoCadastrar');
 
 botaoCadastrar.addEventListener('click', function() {
 
-    const produto = new Produto(nome.value, preco.value, categoria.value, desconto.value);
+    const produto = new Produto(
+        nome.value,
+        preco.value,
+        categoria.value,
+        desconto.value
+    );
 
     estoque.adicionarProduto(produto);
     estoque.exibirNaTela();
-    
+
+    // Limpa os campos depois do cadastro
+    nome.value = "";
+    preco.value = "";
+    categoria.value = "";
+    desconto.value = "";
 });
-
-
-
